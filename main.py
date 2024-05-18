350fc4dbe5787426215d98ed247b0cafd0fd68d9
martin-2k <85652994+martin-2k@users.noreply.github.com>
2024-05-18 09:51:22 -0500

    cacaad

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     """Example Hello World route."""
-    name = os.environ.get("NAME", "World")
+    name = os.environ.get("NAME", "experto en cloud computing")
     return f"Hello {name}!"
 
 
